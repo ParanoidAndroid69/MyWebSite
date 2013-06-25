272f9380ec4a22725b7b3cd5043c2e03c854a1ce
ParanoidAndroid69 <manso.enzo@gmail.com>
2013-06-25 17:14:56 +0200

    navigation bar adjusted to centre

--- include/libraries.php
+++ include/libraries.php
@@ -13,16 +13,16 @@ else
 }
 
 echo <<<_END
-<div id="header">
+
 	    <div id="menu">
         		<ul>
-                <li><a href="$doc_root/index.php" title="HOME">home</a></li>
+                <li><a href="$doc_root/index.php" title="HOME" class=logo>home</a></li>
                 <li><a href="$doc_root/travel/grid.php" title="My Trips"><span>travelling</span></a></li>
                 <li><a href="#">images</a></li>
-                <li><a href="#">travel info</a></li>
-           		</ul>
+                <li><a href="#">words</a></li>
+				<li><a href="#">about</a></li>
+          		</ul>
 		</div>                
-</div>
 _END;
 }
 function get_footer() {
